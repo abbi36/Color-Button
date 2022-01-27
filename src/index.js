1674732c128d0dcbeb3481ba0e1d6b0502ac769d
Abbi Engel <abbi.engel33@gmail.com>
2022-01-27 11:32:52 -0600

    Color button

--- src/index.js
+++ src/index.js
@@ -7,5 +7,22 @@ function changeColor(button) {
     } else if (current == 'orange') {
         button.style.setProperty('background-color', 'yellow');
         current = 'yellow';
+    } else if (current == 'yellow') {
+        button.style.setProperty('background-color', 'green');
+        current = 'green'
+    } else if (current == 'green') {
+        button.style.setProperty('background-color', 'blue');
+        current = 'blue'
+    } else if (current == 'blue') {
+        button.style.setProperty('background-color', 'indigo');
+        current = 'indigo'
+    } else if (current == 'indigo') {
+        button.style.setProperty('background-color', 'violet')
+        current = 'violet'
+    } else if (current == 'violet') {
+        button.style.setProperty('background-color', 'red')
+        current = 'red'
     }
 }
+
+
